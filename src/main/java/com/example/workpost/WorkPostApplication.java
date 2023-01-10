a03e3ee71c828a0f627e1a151043aea8f73f0824
321ru <gerarr999@yandex.ru>
2023-01-10 11:37:40 +0300

    main

---
+++ src/main/java/com/example/workpost/WorkPostApplication.java
@@ -0,0 +1,13 @@
+package com.example.workpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkPostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/workpost/WorkPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
